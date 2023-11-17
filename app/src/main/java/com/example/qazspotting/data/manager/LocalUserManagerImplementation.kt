ad361aac02d19ae84a832533aa351e4fd6980348
Adilet <117200205+adekinho@users.noreply.github.com>
2023-11-17 14:32:19 +0500

    First commit, just to save

---
+++ app/src/main/java/com/example/qazspotting/data/manager/LocalUserManagerImplementation.kt
@@ -0,0 +1,8 @@
+package com.example.qazspotting.data.manager
+
+import android.content.Context
+import com.example.qazspotting.domain.manager.LocalUserManager
+
+class LocalUserManagerImplementation(private val context: Context): LocalUserManager {
+
+}
---
+++ app/src/main/java/com/example/qazspotting/domain/manager/LocalUserManager.kt
@@ -0,0 +1,5 @@
+package com.example.qazspotting.domain.manager
+
+interface LocalUserManager {
+
+}
